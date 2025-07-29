202c675509d98604fa237135912cd05b34e80acb
Jakob Stechow <jakob.stechow@pm.me>
2025-07-29 20:10:03 +0200

    fix: raycast origin should be from player not ZERO

--- scripts/PlayerNode.cs
+++ scripts/PlayerNode.cs
@@ -4,21 +4,18 @@ using System.Numerics;
 
 public partial class PlayerNode : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Console.WriteLine("test");
 		var newPosition = Position;
 		if (Input.IsActionJustPressed("move_right"))
 		{
 			var targetVector = newPosition + new Godot.Vector2(32, 16);
-			var isTargetVectorWall = CheckIfWall(targetVector);
+			var isTargetVectorWall = CheckIfWall(newPosition, targetVector);
 			if (isTargetVectorWall)
 			{
 				return;
@@ -28,7 +25,7 @@ public partial class PlayerNode : Node2D
 		else if (Input.IsActionJustPressed("move_left"))
 		{
 			var targetVector = newPosition + new Godot.Vector2(-32, -16);
-			var isTargetVectorWall = CheckIfWall(targetVector);
+			var isTargetVectorWall = CheckIfWall(newPosition, targetVector);
 			if (isTargetVectorWall)
 			{
 				return;
@@ -38,7 +35,7 @@ public partial class PlayerNode : Node2D
 		else if (Input.IsActionJustPressed("move_down"))
 		{
 			var targetVector = newPosition + new Godot.Vector2(-32, 16);
-			var isTargetVectorWall = CheckIfWall(targetVector);
+			var isTargetVectorWall = CheckIfWall(newPosition, targetVector);
 			if (isTargetVectorWall)
 			{
 				return;
@@ -49,7 +46,7 @@ public partial class PlayerNode : Node2D
 		else if (Input.IsActionJustPressed("move_up"))
 		{
 			var targetVector = newPosition + new Godot.Vector2(32, -16);
-			var isTargetVectorWall = CheckIfWall(targetVector);
+			var isTargetVectorWall = CheckIfWall(newPosition, targetVector);
 			if (isTargetVectorWall)
 			{
 				return;
@@ -59,11 +56,11 @@ public partial class PlayerNode : Node2D
 		Position = newPosition;
 	}
 
-	public bool CheckIfWall(Godot.Vector2 target_vector)
+	public bool CheckIfWall(Godot.Vector2 current_vector, Godot.Vector2 target_vector)
 	{
 		var spaceState = GetWorld2D().DirectSpaceState;
 		// use global coordinates, not local to node
-		var query = PhysicsRayQueryParameters2D.Create(Godot.Vector2.Zero, target_vector);
+		var query = PhysicsRayQueryParameters2D.Create(current_vector, target_vector);
 		var result = spaceState.IntersectRay(query);
 		return result.Count != 0;
 	}
