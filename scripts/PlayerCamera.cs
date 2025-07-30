ec59548642076c86aa68bb167cb0c6b8f35748a2
Jakob Stechow <jakob.stechow@pm.me>
2025-07-30 11:16:05 +0200

    feat: camera zoom

--- scripts/PlayerCamera.cs
+++ scripts/PlayerCamera.cs
@@ -1,63 +1,80 @@
-using Godot;
 using System;
+using Godot;
 
-public partial class PlayerCamera : Node2D
+public partial class PlayerCamera : Camera2D
 {
+	[ExportCategory("Follow Settings")]
+	[Export]
+	private Node2D target;
+	[Export]
+	private int lerp;
 
+	[ExportCategory("Camera Shake Settings")]
+	[Export]
+	private int strength;
 
-    [ExportCategory("Follow Settings")]
-    [Export]
-    private Node2D target;
-    [Export]
-    private int lerp;
+	private double elapsedTime;
+	private double endTime;
 
-    [ExportCategory("Camera Shake Settings")]
-    [Export]
-    private int strength;
+	[Export]
+	public Camera2D camera;
 
-    private double elapsedTime;
-    private double endTime;
+	public void shakeCamera(double duration)
+	{
+		elapsedTime = 0;
+		endTime = duration;
+	}
 
-    public void shakeCamera(double duration)
-    {
-        elapsedTime = 0;
-        endTime = duration;
-    }
+	public override void _Ready()
+	{
+		base._Ready();
 
-    public override void _Ready()
-    {
+		if (target == null)
+		{
+			GD.PrintErr("HEY YOU FUCKER, YOU NEED TO SET SOMETHING FOR THIS TO WORK!");
+		}
 
-        base._Ready();
+	}
 
-        if (target == null)
-        {
-            GD.PrintErr("HEY YOU FUCKER, YOU NEED TO SET SOMETHING FOR THIS TO WORK!");
-        }
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
 
-    }
+		if (elapsedTime < endTime)
+		{
+			Position = new Vector2(
+				Mathf.Lerp(Position.X, target.Position.X + (strength/2 - GD.Randf() * strength), (float)delta * lerp / 2),
+				Mathf.Lerp(Position.Y, target.Position.Y + (strength/2 - GD.Randf() * strength), (float)delta * lerp / 2)
+				);
+		}
+		else
+		{
+			Position = new Vector2(
+				Mathf.Lerp(Position.X, target.Position.X, (float)delta * lerp),
+				Mathf.Lerp(Position.Y, target.Position.Y, (float)delta * lerp)
+				);
+		}
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
+		elapsedTime += delta;
 
-        if (elapsedTime < endTime)
-        {
-            Position = new Vector2(
-                Mathf.Lerp(Position.X, target.Position.X + (strength/2 - GD.Randf() * strength), (float)delta * lerp / 2),
-                Mathf.Lerp(Position.Y, target.Position.Y + (strength/2 - GD.Randf() * strength), (float)delta * lerp / 2)
-                );
-        }
-        else
-        {
-            Position = new Vector2(
-                Mathf.Lerp(Position.X, target.Position.X, (float)delta * lerp),
-                Mathf.Lerp(Position.Y, target.Position.Y, (float)delta * lerp)
-                );
-        }
-
-
-
-        elapsedTime += delta;
-    }
+		if (Input.IsActionJustPressed("mouse_up"))
+		{
+			var currentZoom = camera.Zoom;
+			GD.Print("currentZoom: " + currentZoom);
+			var newZoom = currentZoom + new Vector2(0.5f, 0.5f);
+			camera.Zoom = newZoom;
+		}
+		else if (Input.IsActionJustPressed("mouse_down"))
+		{
+			var currentZoom = camera.Zoom;
+			GD.Print("currentZoom: " + currentZoom);
+			var newZoom = currentZoom - new Vector2(0.5f, 0.5f);
+			if (newZoom.X < 0 || newZoom.Y < 0)
+			{
+				return;
+			}
+			camera.Zoom = newZoom;
+		}
+	}
 
 }
