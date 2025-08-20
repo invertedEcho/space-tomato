e4ead4b48563f2750b394a85bdfb02c75064973e
Jakob Stechow <jakob.stechow@pm.me>
2025-08-20 17:01:20 +0200

    ci: setup editorconfig

--- scripts/HUD.cs
+++ scripts/HUD.cs
@@ -1,46 +1,45 @@
 using Godot;
-using System;
 
 public partial class HUD : CanvasLayer
 {
-	[ExportCategory("References")]
-	[Export]
-	private ProgressBar healthBar;
+    [ExportCategory("References")]
+    [Export]
+    private ProgressBar healthBar;
 
-	[Export]
-	private TextureRect damageIndicator;
+    [Export]
+    private TextureRect damageIndicator;
 
-	[Export]
-	public int selectedHotbarItem;
+    [Export]
+    public int selectedHotbarItem;
 
-	public double elapsedTime = 1;
+    public double elapsedTime = 1;
 
-	[Export]
-	public TextureRect fertilizerTexture;
-	[Export]
-	public TextureRect waterTexture;
+    [Export]
+    public TextureRect fertilizerTexture;
+    [Export]
+    public TextureRect waterTexture;
 
 
-	public override void _Ready()
-	{
-		base._Ready();
+    public override void _Ready()
+    {
+        base._Ready();
 
-		this.Visible = true;
-	}
+        this.Visible = true;
+    }
 
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
 
-		if (elapsedTime == 0)
-		{
-			UglyGlobalState.soundManager.PlaySound(UglyGlobalState.soundManager.getSoundPalette("ALERT"), Vector2.Zero);
-		}
+        if (elapsedTime == 0)
+        {
+            UglyGlobalState.soundManager.PlaySound(UglyGlobalState.soundManager.getSoundPalette("ALERT"), Vector2.Zero);
+        }
 
-		damageIndicator.SelfModulate = new Color(1, 1, 1, Mathf.Lerp(1, 0, (float)elapsedTime * 2));
+        damageIndicator.SelfModulate = new Color(1, 1, 1, Mathf.Lerp(1, 0, (float)elapsedTime * 2));
 
-		elapsedTime += delta;
-		healthBar.Value = UglyGlobalState.player.health;
-	}
+        elapsedTime += delta;
+        healthBar.Value = UglyGlobalState.player.health;
+    }
 
 }
